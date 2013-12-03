b2d9e9f4528d8eab673c3348608991458e127532
Tommy Klein <twklein@andrew.cmu.edu>
2013-12-03 17:52:33 -0500

    Pushed changes to final level

---
+++ Assets/OutlineSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineSpawner : MonoBehaviour {
+	public GameObject outlinePrefab;
+	private int cubeCount = 3;
+	private int cubeScaleJump = 1;
+	private int cubeScaleInit = 5;
+	private GameObject[] cubes;
+	// Use this for initialization
+	void Start () {
+		cubes = new GameObject[cubeCount];
+		int curScale = cubeScaleInit;
+		for (int i = 0; i < cubeCount; i++)
+		{
+			GameObject newOutline = (GameObject)Instantiate (outlinePrefab, Vector3.zero, Quaternion.identity);
+			newOutline.transform.localScale = new Vector3(curScale, curScale, curScale);
+			curScale += cubeScaleJump;
+			cubes[i] = newOutline;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < cubeCount; i++)
+		{
+			//cubes[i].transform.Rotate (new Vector3(cubeCount - i, 0, 0));
+			cubes[i].transform.Rotate((new Vector3(0,cubeCount -i,0)) * Time.deltaTime * Mathf.Min (Time.realtimeSinceStartup / 10, 1.0f));
+		}
+	}
+}
